bd920219e208fa53196584e4c984425fe49a7b71
Alec Wells <alec.wells47@gmail.com>
2019-12-17 10:25:58 -0700

    Display top word and count

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import $ from 'jquery'
+const fetch = require('node-fetch');
 
 $(document).ready(() => {
-  // have fun!
+  async function fetchword() {
+    let response = await fetch(` https://wordwatch-api.herokuapp.com/api/v1/top_word`);
+    let word = await response.json();
+    return word
+  }
+
+
+  $.get('https://wordwatch-api.herokuapp.com/api/v1/top_word', function(data){
+    $('.top-word').append(Object.keys(data.word))
+    $('.word-count').append(Object.values(data.word))
+  })
+
 })
