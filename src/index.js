b4dccb8a6fc2a22bbb53d26d7e2c032d41ba55ee
Alec Wells <alec.wells47@gmail.com>
2019-12-17 11:49:34 -0700

    Started on post

--- src/index.js
+++ src/index.js
@@ -2,16 +2,15 @@ import $ from 'jquery'
 const fetch = require('node-fetch');
 
 $(document).ready(() => {
-  async function fetchword() {
-    let response = await fetch(` https://wordwatch-api.herokuapp.com/api/v1/top_word`);
-    let word = await response.json();
-    return word
-  }
-
 
   $.get('https://wordwatch-api.herokuapp.com/api/v1/top_word', function(data){
     $('.top-word').append(Object.keys(data.word))
     $('.word-count').append(Object.values(data.word))
+  })
+
+  $('#break-button').click(function() {
+        var word = $("#text-sub").val()
+        alert(word)
   })
 
 })
